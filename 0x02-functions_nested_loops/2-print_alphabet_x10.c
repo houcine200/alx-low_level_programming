3bb619c5514b61473d7cb8e7b5b335d2876af5d6
houcine walaq <houcine.walaq@gmail.com>
2023-06-20 07:02:01 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+
+void print_alphabet_x10(void)
+{
+	int line;
+	char letter;
+
+	for (line = 1; line <= 10; line++)
+	{
+		_putchar('\n');
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	}
+	_putchar('\n');
+}
+
