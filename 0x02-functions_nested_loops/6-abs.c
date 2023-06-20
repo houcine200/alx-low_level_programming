d519ba8db76c8b38d8fdde051de1ab607b5a755b
houcine walaq <houcine.walaq@gmail.com>
2023-06-20 10:02:23 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @a: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (a * -1);
+	}
+	else
+	{
+		return (a);
+	}
+}
