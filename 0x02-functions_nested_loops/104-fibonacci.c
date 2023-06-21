7a67a428e393ac2a33da9cf9b467100d5d11db74
houcine walaq <houcine.walaq@gmail.com>
2023-06-21 08:03:47 -0700

    task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,49 @@
 #include <stdio.h>
 /**
- * main - Prints the first 98 Fibonacci numbers
- * starting with 1 and 2, separated by a comma followed by a space.
- * Return: Always 0.
- */
+* main - Prints the first 98 Fibonacci numbers, starting with
+*        1 and 2, separated by a comma followed by a space.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-	int i;
-	long int fib1 = 0, fib2 = 1, sum;
-	long int fib1_hlf1, fib1_hlf2, fib2_hlf1, fib2_hlf2;
-	long int hlf1, hlf2;
+	int c, boolean1, boolean2;
+	long int n1, n2, fn, fn2, n11, n22;
 
-	for (i = 0; i < 92; i++)
+	n1 = 1;
+	n2 = 2;
+	boolean1 =  boolean2 = 1;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 96; c++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
-
-		fib1 = fib2;
-		fib2 = sum;
-	}
-
-	fib1_hlf1 = fib1 / 10000000000;
-	fib2_hlf1 = fib2 / 10000000000;
-	fib1_hlf2 = fib1 % 10000000000;
-	fib2_hlf2 = fib2 % 10000000000;
-
-	for (i = 93; i < 99; i++)
-	{
-		hlf1 = fib1_hlf1 + fib2_hlf1;
-		hlf2 = fib1_hlf2 + fib2_hlf2;
-		if (fib1_hlf2 + fib2_hlf2 > 9999999999)
+		if (boolean1)
 		{
-			hlf1 += 1;
-			hlf2 %= 10000000000;
+			fn = n1 + n2;
+			printf(", %ld", fn);
+			n1 = n2;
+			n2 = fn;
 		}
-
-		printf("%lu%lu", hlf1, hlf2);
-		if (i != 98)
-			printf(", ");
-
-		fib1_hlf1 = fib2_hlf1;
-		fib1_hlf2 = fib2_hlf2;
-		fib2_hlf1 = hlf1;
-		fib2_hlf2 = hlf2;
+		else
+		{
+			if (boolean2)
+			{
+				n11 = n1 % 1000000000;
+				n22 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
+				boolean2 = 0;
+			}
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
+		}
+		if (((n1 + n2) < 0) && boolean1 == 1)
+			boolean1 = 0;
 	}
 	printf("\n");
 	return (0);
