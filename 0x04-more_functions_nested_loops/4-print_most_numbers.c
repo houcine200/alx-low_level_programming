a9aca2c6893b24fa85a1e5616f464f84cc3a79ac
houcine walaq <houcine.walaq@gmail.com>
2023-06-22 04:07:07 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print all numbers except 2 and 4
+ * Return: 0 on success.
+ */
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
+	}
+}
