9175fbf6ab91539d9baaf9cbebb148d75f66ef6f
houcine walaq <houcine.walaq@gmail.com>
2023-06-22 13:16:53 -0700

    task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,37 +1,24 @@
-include "main.h"
+#include "main.h"
 /**
- * print_number - prints a number
- * @n: Input number
- */
+* print_number - prints an integer
+* @n: integer to be printed
+*
+* Return: void
+*/
 void print_number(int n)
 {
-	long length, result, i, temp, exp;
+	unsigned int num;
 
-	result = n;
-	exp = length =  1;
-/*Check negatives*/
-	if (result < 0)
+	if (n < 0)
 	{
-		result *= -1;
 		_putchar('-');
+		num = -n;
 	}
+	else
+		num = n;
 
-/**/
-	temp = result;
-	while (temp >= 10)
-	{
-		length++;
-		temp /= 10;
-	}
+	if (num / 10)
+		print_number(num / 10);
 
-/*Create exponent*/
-	for (i = 1; i < length; i++)
-		exp *= 10;
-/*Main*/
-	while (exp > 1)
-	{
-		_putchar((result / exp) % 10 + '0');
-		exp /= 10;
-	}
-	_putchar(result % 10 + '0');
+	_putchar(num % 10 + '0');
 }
