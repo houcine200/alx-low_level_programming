f8e9efa612349d96109b8d9156ffb7017e324191
houcine walaq <houcine.walaq@gmail.com>
2023-06-22 08:16:59 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - draws a square using multiple terminal lines and #'s.
+ * @size: the number of # to print in each line.
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
