da346713dad890105a46dfe123b1b8d267f28957
houcine walaq <houcine.walaq@gmail.com>
2023-06-22 12:52:17 -0700

    100-pr

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,13 +5,15 @@
  */
 int main (void)
 {
-	long int n = 612852475143;
-	long int i;
+	long n = 612852475143;
+	long i;
 
 	for (i = 2; i < n; i++)
+	{
 		for (;n % i == 0; n /= i)
 		{
 		}
-	printf("%ld\n", n);
+	}
+	printf("%li\n", n);
 	return (0);
 }
