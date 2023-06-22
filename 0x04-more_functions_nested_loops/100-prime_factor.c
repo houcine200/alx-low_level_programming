f4e078230865b53b0184f2e8e0d7b49eba530a19
houcine walaq <houcine.walaq@gmail.com>
2023-06-22 12:56:31 -0700

    taskos100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,17 +3,17 @@
  * main - Prints out the larget prime factor of 612852475143
  * Return:return 0 with success.
  */
-int main (void)
+int main(void)
 {
-	long n = 612852475143;
-	long i;
+	long int n = 612852475143;
+	long int i;
 
 	for (i = 2; i < n; i++)
 	{
-		for (;n % i == 0; n /= i)
+		for (; n % i == 0; n /= i)
 		{
 		}
 	}
-	printf("%li\n", n);
+	printf("%ld\n", n);
 	return (0);
 }
