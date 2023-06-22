3918e2a842888201e5e62db29b7929a2186175fc
houcine walaq <houcine.walaq@gmail.com>
2023-06-22 11:40:57 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_triangle - draws a triangle using multiple terminal lines and #'s.
+ * @size: the height of the triangle.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+		for (i = size; i >= 1; i--)
+		{
+			for (j = 1; j < i; ++j)
+				_putchar(' ');
+			for (j = size; j >= i; --j)
+				_putchar('#');
+			_putchar('\n');
+		}
+	else
+		_putchar('\n')
+}
