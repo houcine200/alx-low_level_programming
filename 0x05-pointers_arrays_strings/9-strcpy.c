4346e7d5a770b46ca4652d88ab32acd9fb91e425
houcine walaq <houcine.walaq@gmail.com>
2023-06-26 12:27:10 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string pointed to by src to buffer pointed to by dest
+ * @dest: the pointer to a string
+ * @src: the pointer to a buffer
+ * Return: the value of the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
