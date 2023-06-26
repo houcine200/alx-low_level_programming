4737ffe2f4f90ce3a6c3eec3909c229e5131e3a8
houcine walaq <houcine.walaq@gmail.com>
2023-06-26 09:51:18 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - Prints out every other char of the string.
+ * @str: input string to print.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			putchar(str[i]);
+	putchar('\n');
+}
