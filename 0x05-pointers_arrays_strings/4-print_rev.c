7b5d4e9441694660cad42dc7b959ccaadc6fb7ee
houcine walaq <houcine.walaq@gmail.com>
2023-06-26 07:40:16 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, followed by a new line.
+ * @s: input string to print.
+ */
+void print_rev(char *s)
+{
+	int l, i;
+
+	for(l = 0; s[l] != '\0'; l++)
+		;
+	for (i = l - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
