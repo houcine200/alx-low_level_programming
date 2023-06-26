4d15c3b7610a83c2551bfceb6a1817f821fc76c3
houcine walaq <houcine.walaq@gmail.com>
2023-06-26 11:26:49 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_array - Print an array up to n.
+ * @a: the array of elements
+ * @n: the number of elements of the array *a.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; a[i] != a[n]; i++)
+	{
+		printf("%d", a[i]);
+		if (a[i] != a[n - 1])
+			printf(", ");
+	}
+	printf("\n");
+}
