9be96ce9b64a9a35d075186a9d05c846e48a2d36
houcine walaq <houcine.walaq@gmail.com>
2023-06-29 09:44:21 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+/**
+ * string_toupper - converts all lowercase letters of a string to uppercase.
+ * @c: pointer to the string to convert
+ * Return: the converted string.
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 32;
+	}
+	return (c);
+}
