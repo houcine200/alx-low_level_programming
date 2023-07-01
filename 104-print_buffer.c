773352fcf43a355469e620a037374b0a96953466
houcine walaq <houcine.walaq@gmail.com>
2023-07-01 10:42:14 -0700

    task 104

--- 104-print_buffer.c
+++ 104-print_buffer.c
@@ -2,44 +2,60 @@
 #include <stdio.h>
 
 /**
- * print_buffer - displays the content of a buffer
- * @buffer: buffer to be displayed
- * @size: size of the buffer
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
  *
  * Return: void
  */
-void print_buffer(char *buffer, int size)
-{
-    int i;
 
-    for (i = 0; i <= (size - 1) / 10 && size; i++)
-    {
-        printf("%08x: ", i * 10);
-        if (i < size / 10)
-        {
-            for (int j = 0; j <= 9; j++)
-            {
-                if (j <= size)
-                    printf("%02x", buffer[i * 10 + j]);
-                else
-                    printf("  ");
-                if (j % 2)
-                    putchar(' ');
-            }
-        }
-        else
-        {
-            for (int k = 0; k <= size % 10 - 1; k++)
-            {
-                if (buffer[i * 10 + k] > 31 && buffer[i * 10 + k] < 127)
-                    putchar(buffer[i * 10 + k]);
-                else
-                    putchar('.');
-            }
-        }
-        putchar('\n');
-    }
-    if (size == 0)
-        putchar('\n');
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j < 10; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
 }
 
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		printf("\n");
+	}
+	if (size == 0)
+		printf("\n");
+}
