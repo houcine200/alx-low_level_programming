8a26fceb57c219263e7416975ffd582e6e6d4194
houcine walaq <houcine.walaq@gmail.com>
2023-07-05 04:12:37 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string with recursion
+ * @s: string to print
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return ('\0');
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
