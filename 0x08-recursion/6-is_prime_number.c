4e244d252c31a72956d88eae9b2098ebdb75e8b9
houcine walaq <houcine.walaq@gmail.com>
2023-07-05 07:31:24 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  * checker - checks recursively the input from is_prime_number
+  * @n: iterator
+  * @b: base number to check
+  * Return: 1 if n is a prime, else return 0 otherwise.
+  */
+int detect(int n, int b)
+{
+	if (n < 2)
+		return (0);
+
+	else if (b * b > n)
+		return (1);
+
+	else if (n % b == 0)
+		return (0);
+
+	return (detect(n, b + 1));
+}
+/**
+  * is_prime_number - checks if the number is a prime number
+  * @n: the number to check
+  * Return: 1 if n is a prime, else return 0 otherwise.
+  */
+int is_prime_number(int n)
+	{
+	return (detect(n, 2));
+	}
