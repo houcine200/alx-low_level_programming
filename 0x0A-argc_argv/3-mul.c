709ce326ec75aae14af782d0827a46cc8784d9ec
houcine walaq <houcine.walaq@gmail.com>
2023-07-07 10:32:42 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,8 +10,9 @@ int main(int argc, char *argv[])
 {
 	if (argc == 3)
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	printf("Error\n");
+		return (0);
 
-return (0);
+	printf("Error\n");
+	return (1);
 }
 
