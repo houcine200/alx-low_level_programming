ec7c1d6ecd1b902cc1b9a2e26a42cba88ff4c32f
houcine walaq <houcine.walaq@gmail.com>
2023-07-10 08:16:03 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -46,8 +46,10 @@ char *str_concat(char *s1, char *s2)
 {
 	char *dest;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = ("");
+	if (s2 == NULL)
+		s2 = ("");
 	dest = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 	if (dest == NULL)
 		return (NULL);
