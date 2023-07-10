ab8e0fe0799de6b10375622f9833b1f82b5f1852
houcine walaq <houcine.walaq@gmail.com>
2023-07-10 05:27:34 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strdup - duplicates a string.
+ * @str: input string to be duplicated.
+ *
+ * Return: NULL if insufficient memory or str = NULL, pointer to duplicate
+ * string if successful.
+ */
+char *_strdup(char *str)
+{
+	char *dest;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	dest = malloc(sizeof(char) * strlen(str) + 1);
+
+	for (i = 0; str[i] != '\0'; i++)
+		dest[i] = str[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
