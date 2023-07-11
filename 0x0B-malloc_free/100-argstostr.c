d30ed6d0d7bdf654e404e1312ed4730746230892
houcine walaq <houcine.walaq@gmail.com>
2023-07-11 08:24:40 -0700

    task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+/**
+ * argstostr - concatenates the arguments to string
+ * @ac: the number of arguments
+ * @av: the arguments
+ * Return: return the string
+ */
 char *argstostr(int ac, char **av)
 {
 	int i, dest_len = 0;
@@ -13,7 +19,7 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 		dest_len = dest_len + strlen(av[i]);
 
-	dest = malloc(sizeof(char) * (dest_len + 1));
+	dest = malloc(sizeof(char) * dest_len + ac + 1);
 
 	if (dest == NULL)
 		return (NULL);
