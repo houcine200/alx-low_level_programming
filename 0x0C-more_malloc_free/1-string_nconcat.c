72158cb4601a8c15a971f24c12c5c1b5ef6ffc15
houcine walaq <houcine.walaq@gmail.com>
2023-07-13 03:28:12 -0700

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * string_nconcat - concatenate two strings up to n bytes.
+ * @s1: source string
+ * @s2: string to truncate up to n bytes
+ * @n: number of bytes to truncate by
+ * Return: pointer to new buffer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *dest;
+	int len;
+
+	if (s1 == NULL)
+		return ("");
+	if (s2 == NULL)
+		return ("");
+
+	if (strlen(s2) <= n)
+		len = strlen(s1) + strlen(s2)  + 1;
+	else
+		len = strlen(s1) + n + 1;
+
+	dest = malloc(sizeof(char) * len);
+
+	if (dest == NULL)
+		return (NULL);
+
+	dest = memcpy(dest, s1, strlen(s1));
+	dest = strncat(dest, s2, n);
+	dest[len - 1] = '\0';
+
+	return (dest);
+}
