8ad6005df2e8525868b27a66d1f69c9e2386cbad
houcine walaq <houcine.walaq@gmail.com>
2023-07-13 14:59:34 -0700

    task 101 corrected

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,35 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include "main.h"
 /**
- * main - main function call
+ * is_number - checks if a string is a number
+ * @str: the string to check
+ * Return: 1 if it's a number, 0 otherwise
+ */
+int is_number(const char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (!isdigit(str[i]))
+			return 0;
+	}
+	return 1;
+}
+/**
+ * main - main function
  * @argc: argument count
- * @argv: 2D array of arguments
+ * @argv: array of arguments
  * Return: return 0 on success, 98 on failure
  */
 int main(int argc, char *argv[])
 {
-	int i, j, mul = 1;
+	int num1, num2;
 
-	if (argc != 3)
+	if (argc != 3 || !is_number(argv[1]) || !is_number(argv[2]))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	else if (argc == 3)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j] != '\0'; j++)
-				if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					exit(98);
-				}
 
-			mul *= atoi(argv[i]);
-		}
-		printf("%d\n", mul);
-	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
 	return (0);
 }
