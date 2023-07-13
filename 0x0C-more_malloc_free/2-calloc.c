3f6ee851490fa1a38e7430a51ba2ddc37e90b164
houcine walaq <houcine.walaq@gmail.com>
2023-07-13 06:57:44 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (sizeof(int) * nmemb); i++)
 		arr[i] = 0;
 
 	return (arr);
