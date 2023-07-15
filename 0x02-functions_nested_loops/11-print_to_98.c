92ae345125a78ed7767f2ff881d13a0471a6ef2f
houcine walaq <houcine.walaq@gmail.com>
2023-07-15 05:31:06 -0700

    11 with putchar

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,27 +8,55 @@
  */
 void print_to_98(int n)
 {
-	int i;
+    int i;
+    if (n <= 98)
+        for (i = n; i <= 98; i++)
+        {
 
-	if (n < 98)
-	{
-		for (i = n; i <= 98; ++i)
-		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
-		}
-	}
-	else if (n > 98)
-	{
-		for (i = n; i >= 98; --i)
-		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
-		}
-	}
-	else
-		printf("%d", n);
-	printf("\n");
+
+            if(i >= 10)
+            {
+                putchar (i / 10 + '0');
+                putchar (i % 10+ '0');
+            }
+
+            else if(i < 10 && i >= 0)
+                putchar (i + '0');
+
+            else if (i < 0)
+            {
+                putchar('-');
+            if (-i >= 10)
+                putchar (-i / 10 + '0');
+            putchar (-i % 10 + '0');
+
+
+            }
+
+            if (i != 98)
+            {
+            putchar (',');
+            putchar (' ');
+            }
+            else
+                putchar ('\n');
+        }
+
+
+    else if (n >= 98)
+        for (i = n; i >= 98; i--)
+        {
+            if (i >= 100)
+                putchar (i / 100 + '0');
+            putchar ((i / 10) % 10 + '0');
+            putchar (i % 10 + '0');
+            if (i != 98)
+            {
+             putchar (',');
+            putchar (' ');
+            }
+            if (i == 98)
+                putchar ('\n');
+        }
+
 }
