fc1a5759131ad6f599c36685ba31e78346e08e36
houcine walaq <houcine.walaq@gmail.com>
2023-07-17 12:00:09 -0700

    task 04

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,8 +23,10 @@ typedef struct dog dog_t;
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
