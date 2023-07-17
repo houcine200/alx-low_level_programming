046b440d159e19ce88f33cb27b63e6f1cea30a80
houcine walaq <houcine.walaq@gmail.com>
2023-07-17 10:56:13 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ void print_dog(struct dog *d)
 	printf("Age: %f\n", d->age);
 
 	if ((*d).owner == NULL)
-		printf("Owner: (nill\n");    
-	printf("Owner: %s\n", (*d).owner);   
+		printf("Owner: (nill\n");
+	printf("Owner: %s\n", (*d).owner);
 }
