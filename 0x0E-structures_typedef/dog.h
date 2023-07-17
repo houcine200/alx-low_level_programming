82a67d2428e9e48a29a8f7123510d2ad84dfbfec
houcine walaq <houcine.walaq@gmail.com>
2023-07-17 08:03:56 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: dog struct containing the name, age and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
