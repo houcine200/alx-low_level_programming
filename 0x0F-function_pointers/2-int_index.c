7fde38d8554dd701f8ed25fc5b38bb04fbb76a84
houcine walaq <houcine.walaq@gmail.com>
2023-07-19 06:26:59 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: pointer to the array
+ * @size: size of the array
+ * @cmp: pointer to the function
+ * Return: index of the first element found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !size || !cmp)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for(i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+			return i;
+	}
+
+	return -1;
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
