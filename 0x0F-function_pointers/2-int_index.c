70b32c15a97d6b20b06be5c3119538feedef3cb9
houcine walaq <houcine.walaq@gmail.com>
2023-07-19 06:34:11 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,17 +10,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || !size || !cmp)
+	if (!array || !cmp)
 		return (-1);
 
 	if (size <= 0)
 		return (-1);
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == 1)
-			return i;
+			return (i);
 	}
 
-	return -1;
+	return (-1);
 }
