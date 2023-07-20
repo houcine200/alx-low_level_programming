35d7b3c89d298ba90b5897a750d84d4e29793d74
houcine walaq <houcine.walaq@gmail.com>
2023-07-20 08:45:23 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,7 +10,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (separator == NULL)
-		return;
+		exit(1);
 
 	va_start(ap, n);
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 #include <string.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
