db6db06216cfbd5ad17d2306691140af53810aec
houcine walaq <houcine.walaq@gmail.com>
2023-07-20 07:30:43 -0700

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sums all arguments
+ * @n: number of arguments
+ * @...: variable list of arguments
+ * Return: sum of all arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+
+	va_list ap;
+	int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, const unsigned int);
+
+	va_end(ap);
+	return (sum);
+}
