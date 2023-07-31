ff663144c7abf1d51bcbd52da24888146a266434
Houcine Walaq <houcine.walaq@gmail.com>
2023-07-31 10:07:20 -0700

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *tmp;
 
-	while(head)
+	while (head != NULL)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		tmp = head;
+		head = head->next;
+		free(tmp);
 	}
 }
