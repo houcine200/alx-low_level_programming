cdc11c51a26a3fe6ae50dc05bc3a45089bfe717c
Houcine Walaq <houcine.walaq@gmail.com>
2023-07-31 05:21:38 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a linked list and sets the head pointer to NULL.
+ *
+ * @head: Pointer to the head of the linked list.
+ */
+
+void free_listint2(listint_t **head)
+{
+	/*
+	 * Check if head is already NULL (list is empty),
+	 * in which case there's nothing to free.
+	 */
+	if (*head == NULL)
+		return;
+
+	listint_t *temp = *head;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	/* Set the head pointer to NULL to indicate that the list is now empty.*/
+	*head = NULL;
+}
+
