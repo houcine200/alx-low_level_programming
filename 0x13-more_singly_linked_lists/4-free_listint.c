ec86a3e15d0058f3ff5260a92377bdd03e394d58
Houcine Walaq <houcine.walaq@gmail.com>
2023-07-31 05:01:31 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - Frees a linked list and sets the head's pointer to NULL.
+ *
+ * @head: Pointer to the head of the linked list.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+
+	while(head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
