4a144b9ca1ea7bf0f255629bfa62f78d9de21f96
Houcine Walaq <houcine.walaq@gmail.com>
2023-07-31 12:55:29 -0700

    task 101 fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,28 +9,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current, *temp;
-	size_t count = 0;
+	const listint_t *curr, *temp;
+	int i = 0;
+	size_t i = 0;
 
 	if (head == NULL)
 	{
 		exit(98);
 	}
-
-	current = head;
-
-	while (current != NULL)
+	curr = head;
+	while (curr != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-		temp = current;
-		current = current->next;
-
-		if (temp < current)
+		printf("[%p] %d\n", (void *)curr, curr->n);
+		i++;
+		temp = curr;
+		curr = curr->next;
+		if (temp < curr)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
+			printf("-> [%p] %d\n", (void *)curr, curr->n);
 			break;
 		}
 	}
-	return (count);
+	return (i);
 }
