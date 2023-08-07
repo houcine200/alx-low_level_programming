a6a44b728246d83fbeba4be5f56bc1d165173018
Houcine Walaq <houcine.walaq@gmail.com>
2023-08-07 11:32:43 -0700

    task 2 comments

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,26 +1,31 @@
 #include "main.h"
-
+/**
+  * append_text_to_file - appends text to a file
+  * @filename: file to append to, if NULL, return -1
+  * @text_content: content to append, if NULL, do not append
+  * Return: 1 on success, -1 on failure
+  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int op, wr;
-    
-    if(!filename)
-        return (-1);
-    
-    op = open(filename, O_CREAT | O_WRONLY | O_APPEND);
-    
-    if(op == -1)
-        return (-1);
-        
-    if (text_content == NULL)
-	    text_content = "";
-	    
+	int op, wr;
+
+	if (!filename)
+		return (-1);
+
+	op = open(filename, O_CREAT | O_WRONLY | O_APPEND);
+
+	if (op == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
 	wr = write(op, text_content, strlen(text_content));
-        
-    if(wr == -1)
-        return (-1);
-    
-    close(op);
-    
-    return (1);
+
+	if (wr == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
 }
