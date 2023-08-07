d60bb533f8b9a77a12510dfb33feb34fe64829f9
Houcine Walaq <houcine.walaq@gmail.com>
2023-08-07 12:26:09 -0700

    task 3 correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,43 +8,71 @@
  */
 int main(int argc, char **argv)
 {
-	int file, file2, w, c, r = -1;
+	int file, file2, w, c, r;
 	char s[1024] = {0};
 
 	/* Check if the correct number of arguments is provided */
-	error97(argc);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
 	/* Open the source file for reading */
 	file = open(argv[1], O_RDONLY);
 	if (file == -1)
-		error98(argv[1]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
 	/* Open or create the destination file for writing */
 	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file2 == -1)
-		error99(argv[2]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file);
+		exit(99);
+	}
 
 	/* Read and write data in chunks of 1024 bytes */
+	r = 1;
 	while (r)
 	{
 		r = read(file, s, 1024);
 		if (r == -1)
-			error98(argv[1]);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close(file);
+			close(file2);
+			exit(98);
+		}
 		if (r)
 		{
 			w = write(file2, s, r);
 			if (w == -1)
-				error99(argv[2]);
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				close(file);
+				close(file2);
+				exit(99);
+			}
 		}
 	}
 
 	/* Close both files */
 	c = close(file);
 	if (c == -1)
-		error100(file);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
 	c = close(file2);
 	if (c == -1)
-		error100(file2);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
+		exit(100);
+	}
 
 	return (0);
 }
