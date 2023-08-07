2f85a493b75208ed9762e06cfed8485c1b069400
Houcine Walaq <houcine.walaq@gmail.com>
2023-08-07 08:17:06 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,43 @@
 #include "main.h"
-
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard out
+ * @filename: name of the file to read
+ * @letters: number of characters to print
+ * Return: 0 on success
+ **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char *buf;
-    ssize_t br, bw;
-    
-    if (filename == NULL)
-        return 0;
-    
-    fd = open(filename, O_RDONLY);
-    
-    if (fd == -1)
-        return(0);
-    
-    buf = malloc(sizeof(char) * letters);
-    
-    if(buf == NULL)
-    {
-        close(fd);
-        return (0);
-    }
-    
-    br = read(fd, buf, letters);
-    buf[br] = '\0';
-    
-    if(br == -1)
-    {
-        free(buf);
-        return (0);
-    }
-    
-    bw =  write(STDOUT_FILENO, buf, br);
-    free(buf);
-    close(fd); 
-    return bw;
+	int fd;
+	char *buf;
+	ssize_t br, bw;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	br = read(fd, buf, letters);
+	buf[br] = '\0';
+
+	if (br == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	bw =	write(STDOUT_FILENO, buf, br);
+	free(buf);
+	close(fd);
+	return (bw);
 }
