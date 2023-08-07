f6c5ef50d1df8d15b18d462b64791fb804a59743
Houcine Walaq <houcine.walaq@gmail.com>
2023-08-07 10:37:21 -0700

    task 1 betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,35 +9,35 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bw;
+	int fd, bw;
 
-    /* Check if the filename is NULL */
-    if (filename == NULL)
-        return (-1);
+	/* Check if the filename is NULL */
+	if (filename == NULL)
+	return (-1);
 
-    /* Open the file with the specified permissions (rw-------) */
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	/* Open the file with the specified permissions (rw-------) */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    /* Check if opening the file failed */
-    if (fd == -1)
-        return (-1);
+	/* Check if opening the file failed */
+	if (fd == -1)
+	return (-1);
 
-    /* If text_content is NULL, create an empty file */
-    if (text_content == NULL)
-        text_content = "";
+	/* If text_content is NULL, create an empty file */
+	if (text_content == NULL)
+	text_content = "";
 
-    /* Write the text_content to the file */
-    bw = write(fd, text_content, strlen(text_content));
+	/* Write the text_content to the file */
+	bw = write(fd, text_content, strlen(text_content));
 
-    /* Check if writing to the file failed */
-    if (bw == -1)
-    {
-        close(fd);
-        return (-1);
-    }
+	/* Check if writing to the file failed */
+	if (bw == -1)
+	{
+	close(fd);
+	return (-1);
+	}
 
-    /* Close the file and return success (1) */
-    close(fd);
-    return (1);
+	/* Close the file and return success (1) */
+	close(fd);
+	return (1);
 }
 
