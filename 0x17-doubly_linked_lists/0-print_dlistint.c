902e0a4654eef0e632b537af426584430d95c507
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-14 04:26:25 -0700

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+  * print_dlistint - prints a doubly linked list
+  * @h: Head of the list
+  * Return: Number of items in the linked list
+  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i;
