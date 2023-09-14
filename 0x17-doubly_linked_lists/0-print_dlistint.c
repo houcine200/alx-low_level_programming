33bc8decb126d2a921db4cc44c32f82c4b19bda2
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-14 04:22:26 -0700

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    int i;
+    
+    for(i = 0; h; i++, h = h->next)
+    {
+       printf("%d\n", h->n);
+    }
+    return i;
+}
