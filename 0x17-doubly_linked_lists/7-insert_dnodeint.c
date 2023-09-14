64a15a54045ed6581cee37f288df6e6b1edcb380
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-14 12:54:58 -0700

    osa

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *mid = malloc(sizeof(dlistint_t));
-	dlistint_t *curr, *after;
+	dlistint_t *curr = *h;
 
 
 	if (!mid)
@@ -19,12 +19,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		mid->next = *h;
-		*h = mid;
-		return (mid);
+		return (add_dnodeint(h, n));
 	}
-	curr = *h;
-
 	for (; curr && idx != 1; idx--)
 		curr = curr->next;
 
@@ -36,9 +32,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	mid->next = curr->next;
 	mid->prev = curr;
 
-	after = curr->next;
-	if (after)
-		after->prev = mid;
+	if (curr->next)
+		curr->next->prev = mid;
 
 	curr->next = mid;
 
