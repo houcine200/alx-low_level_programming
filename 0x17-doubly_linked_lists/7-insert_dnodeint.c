17bea917bfc04fe65eee7c7bf2196bdb003a848d
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-14 07:38:21 -0700

    special case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,9 +25,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	curr = *h;
 
-	for (; idx != 1; idx--)
+	for (; curr && idx != 1; idx--)
 		curr = curr->next;
-
+	
+	if (curr == NULL)
+	{
+		free(mid);
+		return NULL;
+	}
 	mid->next = curr->next;
 	mid->prev = curr;
 
