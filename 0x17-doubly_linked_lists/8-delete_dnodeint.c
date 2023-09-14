db775a54a0d5e0156bdc122aab662f4ee00b2a13
Houcine Walaq <houcine.walaq@gmail.com>
2023-09-14 10:34:30 -0700

    8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+  * delete_dnodeint_at_index - deletes a node at an index
+  * @head: double pointer to the head of the list
+  * @index: index of the node to delete
+  * Return: value of the node deleted
+  */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *curr, *before;
+
+    if(*head == NULL)
+        return (-1);
+
+    curr = *head;
+
+    if(index == 0)
+    {
+        if (curr->next != NULL)
+        {
+            curr->next->prev = NULL;
+        }
+        *head = curr->next;
+        free(curr);
+        return (1);
+    }
+
+
+    for (;curr && index > 0; index--)
+    {
+        curr = curr->next;
+    }
+    if (curr->next == NULL)
+    {
+            return -1;
+    }
+    before = curr->prev;
+    before->next = curr->next;
+    curr->next->prev = before;
+    free(curr);
+    curr = NULL;
+
+    return (1);
+}
