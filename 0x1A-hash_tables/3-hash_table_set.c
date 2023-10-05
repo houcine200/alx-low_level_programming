5f4212598d5a84ae9d596df7c25dc54c3c7b1cc8
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-05 20:37:49 +0100

    break 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,61 +1,81 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * hash_table_set - adds an element to the hash table
- *
- * @ht: hash table to add or update the key/value to
- * @key: key to add to the hash table
- * @value: value associated with the key
- * Return: 1 if it succeeded, 0 otherwise
+ * add_node - Adds a new node to the beginning of a linked list.
+ * @head: Pointer to the head of the linked list.
+ * @key: Key to be added to the new node.
+ * @value: Value to be added to the new node.
+ * Return: Pointer to the new node or NULL on failure.
+ */
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+    hash_node_t *new_node;
+
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->key = strdup(key);
+    if (new_node->key == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->value = strdup(value);
+    if (new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->next = *head;
+    *head = new_node;
+
+    return (new_node);
+}
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table to add/update the key/value in.
+ * @key: The key (cannot be an empty string).
+ * @value: The value associated with the key (duplicated, can be an empty string).
+ * Return: 1 if it succeeded, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *curr, *new_node;
-	char *new_value;
+    unsigned long int index;
+    hash_node_t *current;
 
-	if (!ht || !key || strcmp(key, "") == 0)
-		return (0);
+    if (!ht || !key || strcmp(key, "") == 0)
+        return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
+    index = key_index((const unsigned char *)key, ht->size);
 
-	curr = ht->array[index];
+    current = ht->array[index];
 
-	while (curr)
-	{
-		if (strcmp(curr->key, key) == 0)
-		{
-			new_value = strdup(value);
+    while (current)
+    {
+        if (strcmp(current->key, key) == 0)
+        {
+            char *new_value = strdup(value);
 
-			if (!new_value)
-				return (0);
+            if (!new_value)
+                return (0);
 
-			free(curr->value);
-			curr->value = new_value;
-			return (1);
-		}
-		curr = curr->next;
-	}
-	new_node = malloc(sizeof(hash_node_t));
+            free(current->value);
+            current->value = new_value;
+            return (1);
+        }
+        current = current->next;
+    }
 
-	if (!new_node)
-		return (0);
+    // If no matching key is found, add a new node to the linked list.
+    if (add_node(&(ht->array[index]), key, value) == NULL)
+        return (0);
 
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
-	return (1);
+    return (1);
 }
