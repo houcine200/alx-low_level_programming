9426e3c12025d7cd0b199f4bed6938e6de694962
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-05 20:27:55 +0100

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,7 +6,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     hash_node_t *curr, *new_node;
     char *new_value;
     
-    if (!ht || !key || key == "")
+    if (!ht || !key || strcmp(key, "") == 0)
         return 0;
     
     index = key_index((const unsigned char *)key, ht->size);
