bc0476ec464db04736ae113d02f9fd2e38880c66
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-06 16:32:06 +0100

    6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,25 +1,28 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_delete - frees memory from a hash table
+ * @ht: pointer to hash table
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *curr, *temp;
-    
-    if(!ht)
-        return;
-    
-    for (i = 0; i < ht->size; i++)
-    {
-        curr = ht->array[i];
-        while(curr)
-        {
-            temp = curr->next;
-            free(curr->key);
-            free(curr->value);
-            free(curr);
-            curr = temp;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	unsigned long int i;
+	hash_node_t *curr, *temp;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		while (curr)
+		{
+			temp = curr->next;
+			free(curr->key);
+			free(curr->value);
+			free(curr);
+			curr = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
