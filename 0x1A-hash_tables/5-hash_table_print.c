560734f17b811e193d40ce53e92ab0df339515b1
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-06 15:42:35 +0100

    5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: pointer to the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, flag = 0;
+	hash_node_t *temp;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		for (; temp; temp = temp->next)
+		{
+			if (flag)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flag = 1;
+		}
+
+	}
+	printf("}\n");
+}
