fa0aff77a1533e65db02306f5355e96820c4e8f1
Houcine Walaq <houcine.walaq@gmai.com>
2023-10-06 16:30:17 +0100

    6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *curr, *temp;
+    
+    if(!ht)
+        return;
+    
+    for (i = 0; i < ht->size; i++)
+    {
+        curr = ht->array[i];
+        while(curr)
+        {
+            temp = curr->next;
+            free(curr->key);
+            free(curr->value);
+            free(curr);
+            curr = temp;
+        }
+    }
+    free(ht->array);
+    free(ht);
+}
